d394a228dbd510ba6f7f602a7d4eaf15ffdeab1c
nihadaslanli <nihadsasl@code.edu.az>
2024-03-05 18:56:22 +0400

    feat:add file

--- ConsoleApp11/User.cs
+++ ConsoleApp11/User.cs
@@ -9,18 +9,18 @@ namespace ConsoleApp11
     internal class User
     {
         private string _Name;
-       private int _age;
+        private int _age;
         private string _pasword;
 
 
-         public string Username
+        public string Username
         {
             get { return _Name; }
             set { _Name = value; }
         }
         public int age
         {
-            get { return _age; }  
+            get { return _age; }
             set { _age = value; }
         }
 
@@ -29,23 +29,5 @@ namespace ConsoleApp11
             get { return _pasword; }
             set { _pasword = value; }
         }
-
-        //public User(string name, int age, string pasword)
-        //{
-        //    Console.WriteLine("ad daxil edin:");
-        //    Name = Convert.ToString(Console.ReadLine());
-        //    Console.WriteLine("yasinizi qeyd edin:");
-        //    this.age = Convert.ToInt32(Console.ReadLine());
-        //    Console.WriteLine("pasword daxil edin:");
-        //    this.pasword = Convert.ToString(Console.ReadLine());
-        //}
-        //public void info()
-        //{
-        //    Console.WriteLine($"Name:{this.Name}age:{this.age},pasword:{this.pasword}");
-        //}
-
     }
-
-
-    
 }
