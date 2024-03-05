54e65f49cad8a122b891036177752ef3b1c8aab5
nihadaslanli <nihadsasl@code.edu.az>
2024-03-05 18:17:58 +0400

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("name",20,"pasword");
+            user1.info();
+            
+            
+            
+            
+        }
+    }
+
+
+     
+}
---
+++ ConsoleApp11/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class User
+    {
+        public string Name;
+        public int age;
+        public string pasword;
+
+        public User(string name, int age, string pasword)
+        {
+            Console.WriteLine("ad daxil edin:");
+            Name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("yasinizi qeyd edin:");
+            this.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("pasword daxil edin:");
+            this.pasword = Convert.ToString(Console.ReadLine());
+        }
+        public void info()
+        {
+            Console.WriteLine($"Name:{this.Name},age:{this.age},pasword:{this.pasword}");
+        }
+
+    }
+
+
+    
+}
